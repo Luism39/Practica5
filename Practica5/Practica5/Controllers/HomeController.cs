49930b149d0e064289c3f3f158ed737077f18ce3
Luis Miguel Aquino Cornelio <luism39@hotmail.com>
2019-11-19 18:14:44 -0400

    commit

--- Practica5/Practica5/Controllers/HomeController.cs
+++ Practica5/Practica5/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Practica5.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Datos Personales";
 
             return View();
         }
