6448dba4fa725d071b6ea039da93923b5820df95
Juan <juanernestomarmolejosdanis@gmail.com>
2019-11-20 20:22:15 -0400

    Lector V2 - Version funcional

--- Practica5/Practica5/Controllers/LectorController.cs
+++ Practica5/Practica5/Controllers/LectorController.cs
@@ -40,11 +40,18 @@ namespace Practica5.Controllers
                     {
                         datos.Add(new Data
                         {
-                            Id = Convert.ToInt32(row.Split(',')[0]),
-                            Nombre = row.Split(',')[1],
-                            Pais = row.Split(',')[2]
+                            //Id = Convert.ToInt32row.Split(',')[0]),
+                            C1 = row.Split(',')[0],
+                            C2 = row.Split(',')[1],
+                            C3 = row.Split(',')[2],
+                            C4 = row.Split(',')[3],
+                            C5 = row.Split(',')[4],
+                            C6 = row.Split(',')[5],
+                            C7 = row.Split(',')[6],
+                            C8 = row.Split(',')[7],
+                            C9 = row.Split(',')[8],
                         });
-                    }
+                    }                                                                                                                                                                                                                                                                                                                                               
                 }
             }
             return View(datos);
--- Practica5/Practica5/Models/Data.cs
+++ Practica5/Practica5/Models/Data.cs
@@ -7,8 +7,14 @@ namespace Practica5.Models
 {
     public class Data
     {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Pais { get; set; }
+        public string C1 { get; set; }
+        public string C2 { get; set; }
+        public string C3 { get; set; }
+        public string C4 { get; set; }
+        public string C5 { get; set; }
+        public string C6 { get; set; }
+        public string C7 { get; set; }
+        public string C8 { get; set; }
+        public string C9 { get; set; }
     }
 }
